562e5d27ef141c424e9b6b5eab4fa7e168f4d9b4
bofner <a.bofner@gmail.com>
2025-11-18 00:13:43 +0900

    Site overhauled

--- README.md
+++
@@ -1,3 +0,0 @@
-# bofner.github.io
-
-This is simply where I am hosting my website. Feel free to peruse the code if you'd like, though don't expect anything too fancy.
---
+++ ancient/siteHandler.js
@@ -0,0 +1,19 @@
+function trianglePuzzlePage(){
+    window.location='trianglePuzzle.html';
+}
+
+function homePage(){
+    window.location='../home/home.html';
+}
+
+function bofnerPage(){
+    window.location='bofner.html';
+}
+
+function spaceTonbowPage(){
+    window.location='spaceTonbow.html';
+}
+
+function homePageFromIndex(){
+    window.location='home/home.html';
+}
